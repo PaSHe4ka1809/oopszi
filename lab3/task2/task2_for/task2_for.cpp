fa012a7b66c1be150f19549b79d720905facf3bf
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-13 21:37:52 +0300

    Add files via upload

---
+++ lab3/task2/task2_for/task2_for.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>;
+using namespace std;
+
+double eps, s, res;
+
+double sum2(double eps) {
+	s = 0;
+	for (int i = 0; ; i++) {
+		s += pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		if (abs(s) < eps) break;
+	}
+	return s;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = sum2(eps);
+	cout << "Сумма:" << s << endl;
+}
