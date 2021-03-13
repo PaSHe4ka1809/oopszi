3e16dd21c0bfa2ca5f7901d3f88631985dd6cafe
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-13 21:41:06 +0300

    Add files via upload

---
+++ lab3/task5/task5_do/task5_do.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, k;
+double eps, s, res;
+
+int findFirstNegativeElement(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	do {
+		i++;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		if (s > 0) continue;
+		k = i;
+	} while (abs(s) > eps);
+	return k;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstNegativeElement(eps);
+	cout << "Первый отрицательный член последовательности:" << res << endl;
+}
