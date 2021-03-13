af6a9ed94c82c0221647e407c074907b1fa92737
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-13 21:38:59 +0300

    Add files via upload

---
+++ lab3/task3/task3_while/task3_while.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, n, k;
+double s, res;
+
+void print(int n, int k) {
+	setlocale(LC_ALL, "RUS");
+	i = 0;
+	while (i < n) {
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		i++;
+			if (i % k == 0)	continue;
+			printf("Иттерация: %d ", i);
+			printf(" Результат: %f\n", s);
+	}
+}
+
+void main(int n, int k) {
+	cout << "n:";
+	cin >> n;
+	cout << "k:";
+	cin >> k;
+	print(n, k);
+}
