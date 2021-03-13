0b783e13d645a7e8266cce93b8cba66931a73b71
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-13 21:40:01 +0300

    Add files via upload

---
+++ lab3/task4/task4_for/task4_for.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>;
+using namespace std;
+
+double eps, s, res;
+int k;
+
+int findFirstElement(double eps) {
+	s = 0;
+	for (int i = 0; ; i++) {
+		k = i;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		if (abs(s) < eps) break;
+	}
+	return k;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstElement(eps);
+	cout << "Первый член последовательности:" << res << endl;
+}
---
+++ lab3/task4/task4_while/task4_while.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, k;
+double eps, s, res;
+
+int findFirstElement(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	while (abs(s) > eps) {
+		i++;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		k = i;
+	}
+	return k;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstElement(eps);
+	cout << "Первый член последовательности:" << res << endl;
+}
