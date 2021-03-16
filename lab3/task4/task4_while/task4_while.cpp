6f6ba6e5f94b0a4f49be12b478501760058306f6
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-17 00:53:12 +0300

    Add files via upload

---
+++ lab3/task4/task4_while/task4_while.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, k;
+double eps, res;
+double s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+
+int findFirstElement(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	while (true) {
+		if (abs(s) <= eps) break;
+		i++;
+		k = i;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	}
+	return k;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstElement(eps);
+	cout << "Первый член последовательности:" << res << endl;
+}
