490de9642c4d14517b15e5337c8aaaf00487257a
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-17 00:48:32 +0300

    Add files via upload

---
+++ lab3/task2/task2_do/task2_do.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i;
+double eps, res;
+double s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+
+double sum(double eps) {
+	i = 0;
+	s = 0;
+	do {
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		i++;
+	} while (abs(s) > eps);
+	return s;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = sum(eps);
+	cout << "Сумма:" << s << endl;
+}
---
+++ lab3/task2/task2_while/task2_while.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i;
+double eps, res;
+double s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+
+double sum(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	while (abs(s) > eps) {
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		i++;
+	}
+	return s;
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = sum(eps);
+	cout << "Сумма:" << s << endl;
+}
