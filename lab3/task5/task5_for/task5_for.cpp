fc46cad67a5e1d9dedfd9bd24a6048cea98b74dd
PaSHe4ka1809 <78873093+PaSHe4ka1809@users.noreply.github.com>
2021-03-17 01:13:39 +0300

    Add files via upload

---
+++ lab3/task5/task5_for/task5_for.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>;
+using namespace std;
+
+double eps, s, res;
+int k;
+
+int findFirstNegativeElement(double eps) {
+	s = 0;
+	for (int i = 0; ; i++) {
+		k = i;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		if (s > 0) continue;
+		if (abs(s) <= eps) {
+			return k;
+		}
+	}
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstNegativeElement(eps);
+	cout << "Первый отрицательный член последовательности:" << res << endl;
+}
---
+++ lab3/task5/task5_while/task5_while.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, k;
+double eps, s, res;
+
+int findFirstNegativeElement(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+	while (true) {
+		i++;
+		k = i;
+		s = pow(-1, i) * (pow(2, i + 1) / (pow(2, 2 * i) + 1));
+		if (s > 0) continue;
+		if (abs(s) <= eps) {
+			return k;
+		}
+	}
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstNegativeElement(eps);
+	cout << "Первый отрицательный член последовательности:" << res << endl;
+}
